73e9b7b400a8e9204c97b8a39f4e6399c8cd9b0b
Ivan <159075250+1vengo@users.noreply.github.com>
2024-10-17 17:44:51 +0200

    Update my-project.js

--- src/js/my-project.js
+++ src/js/my-project.js
@@ -1,20 +1,29 @@
 // Массив всех проектов
 import icon from "/img/my-projects/symbol-defs.svg"
 const allProjects = [
-   { img: '/img/my-projects/rectangle-11.jpg', title: 'wallet webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
-  { img: '/img/my-projects/rectangle-12.jpg', title: 'English excellence webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
-  { img: '/img/my-projects/rectangle-9.jpg', title: 'green harvest online store', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
-  { img: '/img/my-projects/first-screen-1.jpg', title: 'starlight studio landing page', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
-  { img: '/img/my-projects/rectangle-10.jpg', title: 'power pulse webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
-  { img: '/img/my-projects/rectangle-4.jpg', title: 'energy flow webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
-  { img: '/img/my-projects/rectangle-5.jpg', title: 'fruitbox online store', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
-  { img: '/img/my-projects/rectangle-6.jpg', title: 'chego jewelry website', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
-  { img: '/img/my-projects/rectangle-7.jpg', title: 'mimino website', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
-  { img: '/img/my-projects/rectangle-8.jpg', title: 'vyshyvanka vibes Landing Page', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
+  { img: image1, title: 'English excellence webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
+  { img: image2, title: 'wallet webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
+  { img: image3, title: 'green harvest online store', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
+  { img: image4, title: 'starlight studio landing page', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
+  { img: image5, title: 'power pulse webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
+  { img: image6, title: 'energy flow webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
+  { img: image7, title: 'fruitbox online store', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
+  { img: image8, title: 'chego jewelry website', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
+  { img: image9, title: 'mimino website', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
+  { img: image10, title: 'vyshyvanka vibes Landing Page', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/project-js-group-2' },
 
 
 ];
-
+import image1 from "../img/my-projects/rectangle-5.jpg";
+import image5 from "../img/my-projects/rectangle-6.jpg";
+import image8 from "../img/my-projects/rectangle-7.jpg";
+import image4 from "../img/my-projects/rectangle-8.jpg";
+import image2 from "../img/my-projects/rectangle-9.jpg";
+import image6 from "../img/my-projects/rectangle-10.jpg";
+import image7 from "../img/my-projects/rectangle-11.jpg";
+import image3 from "../img/my-projects/rectangle-12.jpg";
+import image9 from "../img/my-projects/first-screen-1.jpg";
+import image10 from "../img/my-projects/rectangle-4.jpg";
 let currentIndex = 0;
 const projectsPerLoad = 3;
 
@@ -39,7 +48,6 @@ function loadProjects() {
       <div class="item-text">
         <p class="text">${project.technologies}</p>
         <h3 class="label">${project.title}</h3>
-
         <a href="${project.github}" class="visit" target="_blank">Visit<svg class="visit-icon" width="15" height="15">
         <use href="/img/my-projects/${icon}#icon-visit">
         </svg></a>
