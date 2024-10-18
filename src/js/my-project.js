685e93f66d1c3b642b3ef02cac18c9878c790d4e
mariabykova23 <144001967+mariabykova23@users.noreply.github.com>
2024-10-18 14:29:16 +0300

    project
    icon amendment in project part

--- src/js/my-project.js
+++ src/js/my-project.js
@@ -1,44 +1,88 @@
 // Массив всех проектов
-import icon from "../img/my-projects/symbol-defs.svg"
+import icon from '../img/my-projects//proj-arrow.svg';
 const allProjects = [
-  { img: image1, title: 'English excellence webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/portfolio-group_2' },
-  { img: image2, title: 'wallet webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/portfolio-group_2' },
-  { img: image3, title: 'green harvest online store', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/portfolio-group_2' },
-  { img: image4, title: 'starlight studio landing page', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/portfolio-group_2' },
-  { img: image5, title: 'power pulse webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/portfolio-group_2' },
-  { img: image6, title: 'energy flow webservice ', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/portfolio-group_2' },
-  { img: image7, title: 'fruitbox online store', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/portfolio-group_2' },
-  { img: image8, title: 'chego jewelry website', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/portfolio-group_2' },
-  { img: image9, title: 'mimino website', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/portfolio-group_2' },
-  { img: image10, title: 'vyshyvanka vibes Landing Page', technologies: 'React, JavaScript, Node JS, Git', github: 'https://github.com/NatAn240185/portfolio-group_2' },
-
-
+  {
+    img: image1,
+    title: 'English excellence webservice ',
+    technologies: 'React, JavaScript, Node JS, Git',
+    github: 'https://github.com/NatAn240185/portfolio-group_2',
+  },
+  {
+    img: image2,
+    title: 'wallet webservice ',
+    technologies: 'React, JavaScript, Node JS, Git',
+    github: 'https://github.com/NatAn240185/portfolio-group_2',
+  },
+  {
+    img: image3,
+    title: 'green harvest online store',
+    technologies: 'React, JavaScript, Node JS, Git',
+    github: 'https://github.com/NatAn240185/portfolio-group_2',
+  },
+  {
+    img: image4,
+    title: 'starlight studio landing page',
+    technologies: 'React, JavaScript, Node JS, Git',
+    github: 'https://github.com/NatAn240185/portfolio-group_2',
+  },
+  {
+    img: image5,
+    title: 'power pulse webservice ',
+    technologies: 'React, JavaScript, Node JS, Git',
+    github: 'https://github.com/NatAn240185/portfolio-group_2',
+  },
+  {
+    img: image6,
+    title: 'energy flow webservice ',
+    technologies: 'React, JavaScript, Node JS, Git',
+    github: 'https://github.com/NatAn240185/portfolio-group_2',
+  },
+  {
+    img: image7,
+    title: 'fruitbox online store',
+    technologies: 'React, JavaScript, Node JS, Git',
+    github: 'https://github.com/NatAn240185/portfolio-group_2',
+  },
+  {
+    img: image8,
+    title: 'chego jewelry website',
+    technologies: 'React, JavaScript, Node JS, Git',
+    github: 'https://github.com/NatAn240185/portfolio-group_2',
+  },
+  {
+    img: image9,
+    title: 'mimino website',
+    technologies: 'React, JavaScript, Node JS, Git',
+    github: 'https://github.com/NatAn240185/portfolio-group_2',
+  },
+  {
+    img: image10,
+    title: 'vyshyvanka vibes Landing Page',
+    technologies: 'React, JavaScript, Node JS, Git',
+    github: 'https://github.com/NatAn240185/portfolio-group_2',
+  },
 ];
-import image1 from "../img/my-projects/rectangle-5.jpg";
-import image5 from "../img/my-projects/rectangle-6.jpg";
-import image8 from "../img/my-projects/rectangle-7.jpg";
-import image4 from "../img/my-projects/rectangle-8.jpg";
-import image2 from "../img/my-projects/rectangle-9.jpg";
-import image6 from "../img/my-projects/rectangle-10.jpg";
-import image7 from "../img/my-projects/rectangle-11.jpg";
-import image3 from "../img/my-projects/rectangle-12.jpg";
-import image9 from "../img/my-projects/first-screen-1.jpg";
-import image10 from "../img/my-projects/rectangle-4.jpg";
+import image1 from '../img/my-projects/rectangle-5.jpg';
+import image5 from '../img/my-projects/rectangle-6.jpg';
+import image8 from '../img/my-projects/rectangle-7.jpg';
+import image4 from '../img/my-projects/rectangle-8.jpg';
+import image2 from '../img/my-projects/rectangle-9.jpg';
+import image6 from '../img/my-projects/rectangle-10.jpg';
+import image7 from '../img/my-projects/rectangle-11.jpg';
+import image3 from '../img/my-projects/rectangle-12.jpg';
+import image9 from '../img/my-projects/first-screen-1.jpg';
+import image10 from '../img/my-projects/rectangle-4.jpg';
 let currentIndex = 0;
 const projectsPerLoad = 3;
-
 
 function loadProjects() {
   const projectList = document.getElementById('project-list');
 
-
   for (let i = 0; i < projectsPerLoad; i++) {
     if (currentIndex >= allProjects.length) {
-
       document.getElementById('load-more').style.display = 'none';
       break;
     }
-
 
     const project = allProjects[currentIndex];
     const li = document.createElement('li');
@@ -48,8 +92,8 @@ function loadProjects() {
       <div class="item-text">
         <p class="text">${project.technologies}</p>
         <h3 class="label">${project.title}</h3>
-        <a href="${project.github}" class="visit" target="_blank">Visit<svg class="visit-icon" width="15" height="15">
-        <use href="/img/my-projects/${icon}#icon-visit">
+        <a href="${project.github}" class="visit" target="_blank">Visit<svg class="visit-icon" >
+        <use href="${icon}#icon-visit">
         </svg></a>
       </div>
     `;
@@ -61,6 +105,5 @@ function loadProjects() {
 
 // Начальная загрузка первых 3 проектов
 loadProjects();
-
 
 document.getElementById('load-more').addEventListener('click', loadProjects);
